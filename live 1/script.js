0f62833e802dfd740a5e31ce3815ed8f3c499de0
Ophélie <ophelie_thomas@hotmail.com>
2023-01-16 16:26:31 +0100

    first commit

---
+++ live 1/script.js
@@ -0,0 +1,46 @@
+// Début de mon script 
+
+// Variable :
+// Element nommé qui contient une valeur
+
+// ARRIVÉE DE LET ET CONST : 
+// Ecma script = ESx ...
+// 2015 => ES6
+// var => let / const
+// let / const => non reconnu par les navigateurs
+// Développeurs ES6 /!\ Navigateurs ES5
+// Babel : moulinette pour compiler (traduire) notre ES6 en ES5
+
+
+// les points virgules à la fin de chaque ligne ne sont plus obligatoires
+// le saut de ligne indique une nouvelle instruction
+// points vigurles à mettre entre 2 instructions si elles sont sur la même ligne
+
+// syntaxe : 
+var monNom = "THOMAS"
+let monPrenom = "34"
+const monAge = "Ophélie"  // valeur ne pouvant pas être modifiée
+
+// Les types de variables simples :
+const string = "Je suis une chaîne de caractères"
+const number = 569 // nombre
+const integer = 1256 // nombre entier
+const float = 12.56 // nombre décimal
+const thousand = 1_000_000 // = 1000000
+const boolean = true // true / false
+
+// Les types de variables complexes :
+const arrayInteger = [ 0, 1, 2, 3, 4, 5 ]
+const arrayString = [ "a", "b", "c" ]
+const arrayStringLong = [ 
+    "je suis un tableau", 
+    "c'est plus lisible",
+    "en allant à la ligne", 
+]
+const object = { 
+    nom : "THOMAS",    // paire clé/valeur
+    prenom : "Ophélie",
+    age : 35
+}
+  
+
